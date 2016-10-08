0d2014adc3ccd934a0e890874e275d4af54327c3
miror force <miror force>
2016-10-09 06:39:34 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,37 @@
+![Image](https://github.com/nisrulz/qreader/blob/master/img/github_banner.png)
+
+
+[![Maven Central](https://maven-badges.herokuapp.com/maven-central/com.github.nisrulz/qreader/badge.svg)](https://maven-badges.herokuapp.com/maven-central/com.github.nisrulz/qreader) [![API](https://img.shields.io/badge/API-9%2B-orange.svg?style=flat)](https://android-arsenal.com/api?level=9) <a href="http://www.methodscount.com/?lib=com.github.nisrulz%3Aqreader%3A1.0.6"><img src="https://img.shields.io/badge/Size-10 KB-e91e63.svg"/></a>
+
+[![Android Arsenal](https://img.shields.io/badge/Android%20Arsenal-QREader-green.svg?style=true)](https://android-arsenal.com/details/1/3478) 
+
+[![Twitter Follow](https://img.shields.io/twitter/follow/nisrulz.svg?style=social)](https://twitter.com/nisrulz)
+
+Android library which makes use of Google's Mobile Vision API to enable reading QR Code.
+
+The library is built for simplicity and ease of use. It not only eliminates most boilerplate code for dealing with setting up QR Code reading , but also provides an easy and simple API to retrieve information from QR Code quickly.
+
+# Changelog
+
+Starting with `1.0.4`, Changes exist in the [releases tab](https://github.com/nisrulz/qreader/releases).
+
+# Usage Docs/Wiki
+For more detailed usage, check the [Wiki](https://github.com/nisrulz/qreader/wiki)
+
+# Including in your project
+- QREader is available in the MavenCentral, so getting it as simple as adding it as a dependency
+```gradle
+compile 'com.github.nisrulz:qreader:1.0.6'
+```
+
+# Pull Requests
+I welcome and encourage all pull requests. It usually will take me within 24-48 hours to respond to any issue or request. Here are some basic rules to follow to ensure timely addition of your request:
+  1. Match coding style (braces, spacing, etc.) This is best achieved using CMD+Option+L (Reformat code) on Mac (not sure for Windows) with Android Studio defaults.
+  2. If its a feature, bugfix, or anything please only change code to what you specify.
+  3. Please keep PR titles easy to read and descriptive of changes, this will make them easier to merge :)
+  4. Pull requests _must_ be made against `develop` branch. Any other branch (unless specified by the maintainers) will get rejected.
+  5. Check for existing [issues](https://github.com/nisrulz/qreader/issues) first, before filing an issue.  
+  6. Have fun!
+
+### Created & Maintained By
+[Nishant Srivastava](https://github.com/nisrulz) ([@nisrulz](https://www.twitter.com/nisrulz))
---
+++ library/src/main/java/github/nisrulz/qreader/QREader.java
@@ -0,0 +1,237 @@
+/*
+ * Copyright (C) 2016 Nishant Srivastava
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package github.nisrulz.qreader;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.util.Log;
+import android.util.SparseArray;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
+import java.io.IOException;
+
+/**
+ * QREader Singleton
+ */
+public class QREader {
+  private static final String LOGTAG = "QREader";
+  private CameraSource cameraSource = null;
+  private BarcodeDetector barcodeDetector = null;
+
+  public static final int FRONT_CAM = CameraSource.CAMERA_FACING_FRONT;
+  public static final int BACK_CAM = CameraSource.CAMERA_FACING_BACK;
+
+  private boolean autofocus_enabled;
+  private final int width;
+  private final int height;
+  private final int facing;
+  private boolean cameraRunning = false;
+  private final QRDataListener qrDataListener;
+  private final Context context;
+  private final SurfaceView surfaceView;
+
+  public QREader(Builder builder) {
+    this.autofocus_enabled = builder.autofocus_enabled;
+    this.width = builder.width;
+    this.height = builder.height;
+    this.facing = builder.facing;
+    this.qrDataListener = builder.qrDataListener;
+    this.context = builder.context;
+    this.surfaceView = builder.surfaceView;
+  }
+
+  /**
+   * Init.
+   */
+  public void init() {
+    if (!hasAutofocus(context)) {
+      Log.e(LOGTAG, "Do not have autofocus feature, disabling autofocus feature in the library!");
+      autofocus_enabled = false;
+    }
+
+    if (!hasCameraHardware(context)) {
+      Log.e(LOGTAG, "Does not have camera hardware!");
+      return;
+    }
+    if (!checkCameraPermission(context)) {
+      Log.e(LOGTAG, "Do not have camera permission!");
+      return;
+    }
+
+    // Setup Barcodedetector
+    barcodeDetector =
+        new BarcodeDetector.Builder(context).setBarcodeFormats(Barcode.QR_CODE).build();
+
+    if (barcodeDetector.isOperational()) {
+      barcodeDetector.setProcessor(new Detector.Processor<Barcode>() {
+        @Override public void release() {
+
+        }
+
+        @Override public void receiveDetections(Detector.Detections<Barcode> detections) {
+          final SparseArray<Barcode> barcodes = detections.getDetectedItems();
+          if (barcodes.size() != 0 && qrDataListener != null) {
+            qrDataListener.onDetected(barcodes.valueAt(0).displayValue);
+          }
+        }
+      });
+    } else {
+      Log.e(LOGTAG, "Barcode recognition libs are not downloaded and are not operational");
+    }
+
+    // Setup Camera
+    cameraSource =
+        new CameraSource.Builder(context, barcodeDetector).setAutoFocusEnabled(autofocus_enabled)
+            .setFacing(facing)
+            .setRequestedPreviewSize(width, height)
+            .build();
+  }
+
+  /**
+   * Start.
+   */
+  public void start() {
+    if (surfaceView != null && surfaceHolderCallback != null) {
+      surfaceView.getHolder().addCallback(surfaceHolderCallback);
+    }
+  }
+
+  private SurfaceHolder.Callback surfaceHolderCallback = new SurfaceHolder.Callback() {
+    @Override public void surfaceCreated(SurfaceHolder surfaceHolder) {
+      startCameraView(context, cameraSource, surfaceView);
+    }
+
+    @Override public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+    }
+
+    @Override public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+      stop();
+    }
+  };
+
+  private void startCameraView(Context context, CameraSource cameraSource,
+      SurfaceView surfaceView) {
+    try {
+
+      if (ActivityCompat.checkSelfPermission(context, Manifest.permission.CAMERA)
+          != PackageManager.PERMISSION_GRANTED) {
+        Log.e(LOGTAG, "Permission not granted!");
+        return;
+      } else if (!cameraRunning && cameraSource != null && surfaceView != null) {
+        cameraSource.start(surfaceView.getHolder());
+        cameraRunning = true;
+      }
+    } catch (IOException ie) {
+      Log.e(LOGTAG, ie.getMessage());
+      ie.printStackTrace();
+    }
+  }
+
+  /**
+   * Stop camera
+   */
+  public void stop() {
+    try {
+      if (surfaceView != null && surfaceHolderCallback != null) {
+        surfaceView.getHolder().removeCallback(surfaceHolderCallback);
+      }
+      if (cameraRunning && cameraSource != null) {
+        cameraSource.stop();
+        cameraRunning = false;
+      }
+    } catch (Exception ie) {
+      Log.e(LOGTAG, ie.getMessage());
+      ie.printStackTrace();
+    }
+  }
+
+  /**
+   * Release and cleanup qreader.
+   */
+  public void releaseAndCleanup() {
+    stop();
+    if (cameraSource != null) {
+      cameraSource.release();
+      cameraSource = null;
+    }
+  }
+
+  private boolean checkCameraPermission(Context context) {
+    String permission = Manifest.permission.CAMERA;
+    int res = context.checkCallingOrSelfPermission(permission);
+    return (res == PackageManager.PERMISSION_GRANTED);
+  }
+
+  private boolean hasCameraHardware(Context context) {
+    return context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA);
+  }
+
+  private boolean hasAutofocus(Context context) {
+    return context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_AUTOFOCUS);
+  }
+
+  public static class Builder {
+    private boolean autofocus_enabled;
+    private int width;
+    private int height;
+    private int facing;
+    private QRDataListener qrDataListener;
+    private Context context;
+    private SurfaceView surfaceView;
+
+    public Builder(Context context, SurfaceView surfaceView, QRDataListener qrDataListener) {
+      this.autofocus_enabled = true;
+      this.width = 800;
+      this.height = 800;
+      this.facing = BACK_CAM;
+      this.qrDataListener = qrDataListener;
+      this.context = context;
+      this.surfaceView = surfaceView;
+    }
+
+    public Builder enableAutofocus(boolean autofocus_enabled) {
+      this.autofocus_enabled = autofocus_enabled;
+      return this;
+    }
+
+    public Builder width(int width) {
+      this.width = width;
+      return this;
+    }
+
+    public Builder height(int height) {
+      this.height = height;
+      return this;
+    }
+
+    public Builder facing(int facing) {
+      this.facing = facing;
+      return this;
+    }
+
+    public QREader build() {
+      return new QREader(this);
+    }
+  }
+}
+
